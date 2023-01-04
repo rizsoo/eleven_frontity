03d6d6d97ceaae232258d45aebabf7b95150a3a4
rizsoo <89772889+rizsoo@users.noreply.github.com>
2023-01-04 11:08:37 +0100

    dangerous

--- packages/eleven-theme/src/components/Popup.js
+++ packages/eleven-theme/src/components/Popup.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { connect, styled } from "frontity"
-import HTMLReactParser from "html-react-parser"
 
 const Popup = ({post}) => {
   return (
     <PopUp>
-        {post.acf.location && HTMLReactParser(post.acf.location)}
+        <div dangerouslySetInnerHTML={{__html: post.acf.location}}></div>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqApSpCIYpYHyJzsOkmBNQjaGyxN9JtTU_thDY-l2uyVWRpQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Betöltés…</iframe>
     </PopUp>
   )
